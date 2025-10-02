cf4b1597e81766dff9c0799e8dcad55d1c8acd7b
Gabriel Brandão <dev.gbrandao@gmail.com>
2025-10-01 22:23:55 -0300

    feature: implementando funçao junto com matriz

---
+++ funcao&matriz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int preenchermatriz(int matriz [3][3]);
+
+int main (){
+ int minhamatriz[3][3];
+
+ preenchermatriz(minhamatriz);
+
+ printf("-----Matriz Preenchida-----\n");
+  for(int i=0;i<3;i++){
+    for (int j=0; j<3; j++){
+        printf(" %d", minhamatriz[i][j]);
+    }
+   printf("\n"); 
+  }
+
+return 0;
+
+}
+
+int preenchermatriz(int matriz [3][3]){
+ printf("Digite 9 numeros para preencher a matriz 3x3:\n");
+ for (int i=0; i<3; i++){
+    for (int j=0; j<3; j++){
+        printf("Digito [%d][%d]: ", i,j);
+         scanf("%d", &matriz[i][j]);
+    }
+ }
+
+}
